9ea5a11145fb1635c52635e35dd7dd01c6473c1b
Herley Antoine <ffast2000@gmail.com>
2025-08-10 13:17:03 -0400

    a

--- src/components/shared/Toolbar.tsx
+++ src/components/shared/Toolbar.tsx
@@ -1,152 +1,152 @@
-'use client';
+// 'use client';
 
-import React from "react";
-import { type Editor } from "@tiptap/react";
-import {
-  Bold,
-  Strikethrough,
-  Italic,
-  List,
-  ListOrdered,
-  Heading1,
-  Heading2,
-  Heading3,
-  Quote,
-  Undo,
-  Redo,
-  Code,
-  Image as ImageIcon,
-  CodepenIcon,
-  Heading4,
-  Heading5,
-  Heading6,
-} from "lucide-react";
+// import React from "react";
+// import { type Editor } from "@tiptap/react";
+// import {
+//   Bold,
+//   Strikethrough,
+//   Italic,
+//   List,
+//   ListOrdered,
+//   Heading1,
+//   Heading2,
+//   Heading3,
+//   Quote,
+//   Undo,
+//   Redo,
+//   Code,
+//   Image as ImageIcon,
+//   CodepenIcon,
+//   Heading4,
+//   Heading5,
+//   Heading6,
+// } from "lucide-react";
 
-type Props = {
-  editor: Editor | null;
-};
+// type Props = {
+//   editor: Editor | null;
+// };
 
-const Toolbar = ({ editor }: Props) => {
-  if (!editor) {
-    return null;
-  }
+// const Toolbar = ({ editor }: Props) => {
+//   if (!editor) {
+//     return null;
+//   }
 
-  // const addImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   const file = event.target.files?.[0];
-  //   if (file) {
-  //     const reader = new FileReader();
-  //     reader.onload = () => {
-  //       const url = reader.result as string;
-  //       editor.chain().focus().setImage({ src: url }).run();
-  //     };
-  //     reader.readAsDataURL(file);
-  //   }
-  // };
+//   // const addImage = (event: React.ChangeEvent<HTMLInputElement>) => {
+//   //   const file = event.target.files?.[0];
+//   //   if (file) {
+//   //     const reader = new FileReader();
+//   //     reader.onload = () => {
+//   //       const url = reader.result as string;
+//   //       editor.chain().focus().setImage({ src: url }).run();
+//   //     };
+//   //     reader.readAsDataURL(file);
+//   //   }
+//   // };
 
-  return (
-    <div className="flex flex-wrap gap-2">
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={!editor.can().chain().focus().toggleBold().run()}
-        className={editor.isActive("bold") ? "is-active" : ""}
-      >
-        <Bold className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={!editor.can().chain().focus().toggleItalic().run()}
-        className={editor.isActive("italic") ? "is-active" : ""}
-      >
-        <Italic className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        disabled={!editor.can().chain().focus().toggleStrike().run()}
-        className={editor.isActive("strike") ? "is-active" : ""}
-      >
-        <Strikethrough className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        disabled={!editor.can().chain().focus().toggleCode().run()}
-        className={editor.isActive("code") ? "is-active" : ""}
-      >
-        <Code className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
-      >
-        <Heading1 className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
-      >
-        <Heading2 className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
-      >
-        <Heading3 className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        className={editor.isActive("heading", { level: 4 }) ? "is-active" : ""}
-      >
-        <Heading4 className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-        className={editor.isActive("heading", { level: 5 }) ? "is-active" : ""}
-      >
-        <Heading5 className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-        className={editor.isActive("heading", { level: 6 }) ? "is-active" : ""}
-      >
-        <Heading6 className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={editor.isActive("bulletList") ? "is-active" : ""}
-      >
-        <List className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={editor.isActive("orderedList") ? "is-active" : ""}
-      >
-        <ListOrdered className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={editor.isActive("codeBlock") ? "is-active" : ""}
-      >
-        <CodepenIcon className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={editor.isActive("blockquote") ? "is-active" : ""}
-      >
-        <Quote className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().undo().run()}
-        disabled={!editor.can().chain().focus().undo().run()}
-      >
-        <Undo className="w-6 h-6" />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().redo().run()}
-        disabled={!editor.can().chain().focus().redo().run()}
-      >
-        <Redo className="w-6 h-6" />
-      </button>
-    </div>
-  );
-};
+//   return (
+//     <div className="flex flex-wrap gap-2">
+//       <button
+//         onClick={() => editor.chain().focus().toggleBold().run()}
+//         disabled={!editor.can().chain().focus().toggleBold().run()}
+//         className={editor.isActive("bold") ? "is-active" : ""}
+//       >
+//         <Bold className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleItalic().run()}
+//         disabled={!editor.can().chain().focus().toggleItalic().run()}
+//         className={editor.isActive("italic") ? "is-active" : ""}
+//       >
+//         <Italic className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleStrike().run()}
+//         disabled={!editor.can().chain().focus().toggleStrike().run()}
+//         className={editor.isActive("strike") ? "is-active" : ""}
+//       >
+//         <Strikethrough className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleCode().run()}
+//         disabled={!editor.can().chain().focus().toggleCode().run()}
+//         className={editor.isActive("code") ? "is-active" : ""}
+//       >
+//         <Code className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+//         className={editor.isActive("heading", { level: 1 }) ? "is-active" : ""}
+//       >
+//         <Heading1 className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+//         className={editor.isActive("heading", { level: 2 }) ? "is-active" : ""}
+//       >
+//         <Heading2 className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+//         className={editor.isActive("heading", { level: 3 }) ? "is-active" : ""}
+//       >
+//         <Heading3 className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+//         className={editor.isActive("heading", { level: 4 }) ? "is-active" : ""}
+//       >
+//         <Heading4 className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+//         className={editor.isActive("heading", { level: 5 }) ? "is-active" : ""}
+//       >
+//         <Heading5 className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+//         className={editor.isActive("heading", { level: 6 }) ? "is-active" : ""}
+//       >
+//         <Heading6 className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleBulletList().run()}
+//         className={editor.isActive("bulletList") ? "is-active" : ""}
+//       >
+//         <List className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleOrderedList().run()}
+//         className={editor.isActive("orderedList") ? "is-active" : ""}
+//       >
+//         <ListOrdered className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+//         className={editor.isActive("codeBlock") ? "is-active" : ""}
+//       >
+//         <CodepenIcon className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().toggleBlockquote().run()}
+//         className={editor.isActive("blockquote") ? "is-active" : ""}
+//       >
+//         <Quote className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().undo().run()}
+//         disabled={!editor.can().chain().focus().undo().run()}
+//       >
+//         <Undo className="w-6 h-6" />
+//       </button>
+//       <button
+//         onClick={() => editor.chain().focus().redo().run()}
+//         disabled={!editor.can().chain().focus().redo().run()}
+//       >
+//         <Redo className="w-6 h-6" />
+//       </button>
+//     </div>
+//   );
+// };
 
-export default Toolbar;
+// export default Toolbar;

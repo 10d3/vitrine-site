22955f2a04f5d73d56c90aff54a931e62d65ec06
Herley Antoine <ffast2000@gmail.com>
2025-10-01 12:34:08 -0400

    refactor(auth): replace anchor tag with Next.js Link component
    Using Next.js Link component for client-side navigation improves performance by prefetching pages in the background

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -7,12 +7,13 @@ export const metadata: Metadata = {
 
 import { SignupForm } from "@/components/signup-form";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function SignUpPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="/" className="flex items-center gap-2 self-center font-medium">
+        <Link href="/" className="flex items-center gap-2 self-center font-medium">
           <Image
             className=" w-32 h-fit"
             src={"/logo.svg"}
@@ -20,7 +21,7 @@ export default function SignUpPage() {
             width={1000}
             height={1000}
           />
-        </a>
+        </Link>
         <SignupForm />
       </div>
     </div>
